20c509e3162520fc0449cf84d22c9532c8d2ed6d
djteiji <djteiji@gmail.com>
2022-07-16 12:48:56 -0700

    add resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SampleResume from './SampleResume.pdf';
 
 function Resume() {
     return (
@@ -7,10 +8,56 @@ function Resume() {
         <div>
             <ul>
                 <li>
-                    <p>React.JS</p>
+                    <p>React</p>
+                </li>
+                <li>
+                    <p>Bootstrap</p>
+                </li>
+                <li>
+                    <p>responsive design</p>
+                </li>
+                <li>
+                    <p>JQuery</p>
+                </li>
+                <li>
+                    <p>Javascript</p>
+                </li>
+                <li>
+                    <p>CSS</p>
+                </li>
+                <li>
+                    <p>HTML</p>
                 </li>
             </ul>
         </div>
+        <h1>Back End</h1>
+        <div>
+            <ul>
+                <li>
+                    <p>APIs</p>
+                </li>
+                <li>
+                    <p>Node</p>
+                </li>
+                <li>
+                    <p>Express</p>
+                </li>
+                <li>
+                    <p>MySQL, Sequelize</p>
+                </li>
+                <li>
+                    <p>MongoDB, Mongoose</p>
+                </li>
+                <li>
+                    <p>REST</p>
+                </li>
+                <li>
+                    <p>GraphQL</p>
+                </li>
+            </ul>
+        </div>
+
+        <a href={SampleResume} download>Download Resume</a>
   
       </section>
     );
